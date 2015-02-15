f7c14e01848cbdbe1dc93d47db691982034a1513
Artem Timoshenko <made-in-france@yandex.ru>
2015-02-15 21:42:59 +0300

    Страница "Новости"

---
+++ js/news.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $(".openBlockButton").click(function() {
+
+        var parent = $(this).parent();
+        if(parent.hasClass("opened")){
+            parent.removeClass("opened");
+        } else {
+            parent.addClass("opened");
+        }
+        //$(".openBlockContent").css({"height":"auto"});
+        //$(".openBlockButton").css({"height":"auto"});
+
+    });
+});
