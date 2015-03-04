69f00811579ea6ec144d82672ff0705f8d69315b
Ilya Sivakov <ilyasivakov@gmail.com>
2015-03-04 23:34:21 +0300

    123

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
-    $(".openBlockButton").click(function() {
+	$(".openBlockButton").click(function() {
 
-        var parent = $(this).parent();
-        if(parent.hasClass("opened")){
-            parent.removeClass("opened");
-        } else {
-            parent.addClass("opened");
-        }
-        //$(".openBlockContent").css({"height":"auto"});
-        //$(".openBlockButton").css({"height":"auto"});
+		var parent = $(this).parent();
+		if(parent.hasClass("opened")){
+			parent.removeClass("opened");
+		} else {
+			parent.addClass("opened");
+		}
+		//$(".openBlockContent").css({"height":"auto"});
+		//$(".openBlockButton").css({"height":"auto"});
 
-    });
+	});
+
+	$('.bxsliderHistory').bxSlider();
 });
